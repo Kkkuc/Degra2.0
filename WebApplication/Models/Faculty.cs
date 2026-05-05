eefb5fc990c3621f62d1baf3968028433ff8dfbd
Kkkuc <91897@student.pb.edu.pl>
2026-05-05 21:29:09 +0200

    Successfull FacultyChanges Migration

--- WebApplication/Models/Faculty.cs
+++ WebApplication/Models/Faculty.cs
@@ -7,6 +7,6 @@ public class Faculty
     [Key] public int Id { get; set; }
     [Required, MaxLength(100)] public string Name { get; set; }
     [MaxLength(10)] public string Abbreviation { get; set; }
-    public virtual ICollection<FieldOfStudy> FieldsOfStudy { get; set; } 
-    public virtual ICollection<Building> Buildings { get; set; }
+    public virtual ICollection<FieldOfStudy>? FieldsOfStudy { get; set; } 
+    public virtual ICollection<Building>? Buildings { get; set; }
 }
