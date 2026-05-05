9826ff3f5f8273811c1e5c56f719dd27875a124f
Kkkuc <91897@student.pb.edu.pl>
2026-05-05 21:22:54 +0200

    Successfull AcademicYearChanges migration.
    Working crud for AcademicYearChanges

--- WebApplication/Models/AcademicYear.cs
+++ WebApplication/Models/AcademicYear.cs
@@ -9,7 +9,7 @@ public class AcademicYear
     [Required(ErrorMessage = "Year name is required"), MaxLength(20), Display(Name = "Year name")]
     public string Name { get; set; } // e.g., "2025/2026"
 
-    public DateOnly StartDate { get; set; }
-    public DateOnly EndDate { get; set; }
+    [DataType(DataType.Date)] public DateOnly StartDate { get; set; }
+    [DataType(DataType.Date)] public DateOnly EndDate { get; set; }
     public virtual ICollection<Semester>? Semesters { get; set; }
 }
