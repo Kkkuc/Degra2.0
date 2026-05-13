8b13ffc275e4b90e50115324df5d745c2b2c470d
Kkkuc <91897@student.pb.edu.pl>
2026-05-13 08:59:04 +0200

    ScheduleChangesChanges Successful migration

---
+++ WebApplication/Migrations/20260513065749_ScheduleChangeChanges.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication.Migrations
+{
+    /// <inheritdoc />
+    public partial class ScheduleChangeChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ChangeType",
+                table: "ScheduleChanges");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ChangeDate",
+                table: "ScheduleChanges",
+                type: "TIMESTAMP(7)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "TIMESTAMP(7)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ChangeDate",
+                table: "ScheduleChanges",
+                type: "TIMESTAMP(7)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "TIMESTAMP(7)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ChangeType",
+                table: "ScheduleChanges",
+                type: "NVARCHAR2(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
