0b57ad4f51405583f86da68354692f846f572a44
ThePlayMK <theplaymk@gmail.com>
2026-06-10 00:17:20 +0200

    Migration

---
+++ WebApplication/Migrations/20260609220417_UpdateModelChanges.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateModelChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AcademicTitle",
+                table: "Teachers",
+                type: "NVARCHAR2(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR2(50)",
+                oldMaxLength: 50);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AcademicTitle",
+                table: "Teachers",
+                type: "NVARCHAR2(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR2(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+        }
+    }
+}
